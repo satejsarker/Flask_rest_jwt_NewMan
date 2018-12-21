20449dd01172d0d41f29de074678017bdcb9be8f
SATEJ SARKER <satejall@gmail.com>
2018-12-22 02:52:44 +0600

    lamda fucntion error handeling implemented without jwt authentication

--- app.py
+++ app.py
@@ -13,17 +13,27 @@ items=[
 
 class Items(Resource):
     def get(self,name):
-        for iteam in items:
-            if(iteam['name']==name):
-                return  iteam
-        return {'iteam':None},404 #not found Status
-    def post(self,name):
-        data=request.get_json(force=True)#if header is not specified
-        #also can be used as silent=True
+        # for iteam in items:
+        #     if(iteam['name']==name):
+        #         return  iteam
+        #using filter
+        iteam=next(filter(lambda x:x['name']==name,items),None)##one item for that next onely one iteam so None for avoid error
+        return {'iteam':iteam},200 if iteam else 404 #not found Status
 
-        item={'name':name,"price":data['price']}
-        items.append(item)
-        return item,201 #creating status
+
+    def post(self,name):
+        if(next(filter(lambda x:x['name']==name,items),None)) is not None:
+            return {"message":"An Iteam with name '{}' already exist ".format(name)} , 400
+
+        else:
+            data = request.get_json(force=True)  # if header is not specified
+            # also can be used as silent=True
+
+            item = {'name': name, "price": data['price']}
+            items.append(item)
+            return item, 201
+
+         #creating status
 
 
 class IteamList(Resource):

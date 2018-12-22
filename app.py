d998a39204a3e0e46570ff43732f61e0331ad7f9
SATEJ SARKER <satejall@gmail.com>
2018-12-22 17:24:44 +0600

    jwt implemented ,put ,delete implemented with reqparse

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask,jsonify,request
-from flask_restful import  Resource,Api
-
+from flask_restful import  Resource,Api,reqparse
+from flask_jwt import  JWT,jwt_required
+from Security import  authenticate,identity
 
 app = Flask(__name__)
+app.secret_key='satej@)((!!!klnvlknsdfiasdasdmnnjan$$%'
+
 api=Api(app)
+jwt=JWT(app,authenticate,identity)
 
 ##demo in maemory data
 
@@ -12,6 +16,13 @@ items=[
 ]
 
 class Items(Resource):
+    parse = reqparse.RequestParser()
+    parse.add_argument('price',
+                       type=float,
+                       required=True,
+                       help="this fild is Required"
+                       )
+    @jwt_required()
     def get(self,name):
         # for iteam in items:
         #     if(iteam['name']==name):
@@ -26,7 +37,7 @@ class Items(Resource):
             return {"message":"An Iteam with name '{}' already exist ".format(name)} , 400
 
         else:
-            data = request.get_json(force=True)  # if header is not specified
+            data = Items.parse.parse_args() # if header is not specified
             # also can be used as silent=True
 
             item = {'name': name, "price": data['price']}
@@ -34,7 +45,20 @@ class Items(Resource):
             return item, 201
 
          #creating status
+    def delete(self,name):
+        global items
+        items=list(filter(lambda x:x['name']!=name,items))
+        return {'message':'item deleted'}
 
+    def put(self,name):
+
+        data=Items.parse.parse_args()
+        item=next(filter(lambda x:x['name']==name,items),None)
+        if item is None:
+            item={"name":name,"price":data['price']}
+        else:
+            item.update(data)
+        return item
 
 class IteamList(Resource):
     def get(self):
--- user.py
+++ user.py
@@ -4,32 +4,32 @@ class  User:
         self.id =  _id
         self.username = username
         self.password = password
-    @classmethod
-    def find_by_username(cls,username):
-        connection= sqlite3.connect('database.db')
-        cursor=connection.cursor()
-        qurry='select * from users where username=?'
-        result=cursor.execute(qurry,(username,))
-        row=result.fetchone()
-        if row:
-            #simplefy the class methord and class name
-            user=cls(*row) #return the complete row
-        else:
-            user=None
-        connection.close()
-        return user
-
-    @classmethod
-    def find_by_id (cls, _id):
-        connection = sqlite3.connect('database.db')
-        cursor = connection.cursor()
-        qurry = 'select * from users where id=?'
-        result = cursor.execute(qurry, (_id,))
-        row = result.fetchone()
-        if row:
-            # simplefy the class methord and class name
-            user = cls(*row)  # return the complete row
-        else:
-            user = None
-        connection.close()
-        return user
+    # @classmethod
+    # def find_by_username(cls,username):
+    #     connection= sqlite3.connect('database.db')
+    #     cursor=connection.cursor()
+    #     qurry='select * from users where username=?'
+    #     result=cursor.execute(qurry,(username,))
+    #     row=result.fetchone()
+    #     if row:
+    #         #simplefy the class methord and class name
+    #         user=cls(*row) #return the complete row
+    #     else:
+    #         user=None
+    #     connection.close()
+    #     return user
+    #
+    # @classmethod
+    # def find_by_id (cls, _id):
+    #     connection = sqlite3.connect('database.db')
+    #     cursor = connection.cursor()
+    #     qurry = 'select * from users where id=?'
+    #     result = cursor.execute(qurry, (_id,))
+    #     row = result.fetchone()
+    #     if row:
+    #         # simplefy the class methord and class name
+    #         user = cls(*row)  # return the complete row
+    #     else:
+    #         user = None
+    #     connection.close()
+    #     return user
